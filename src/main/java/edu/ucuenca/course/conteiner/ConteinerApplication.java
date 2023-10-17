129aea8d51d509e889b578708bfe1d54610310ec
shejo8511 <shejo8511@hotmail.com>
2023-10-17 09:40:59 -0500

    Primer Commit

---
+++ src/main/java/edu/ucuenca/course/conteiner/ConteinerApplication.java
@@ -0,0 +1,13 @@
+package edu.ucuenca.course.conteiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConteinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConteinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ucuenca/course/conteiner/ConteinerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucuenca.course.conteiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConteinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
